668f5acdccebc6298dea8e6c64f399b1b0b74a76
Elioenay <elionay407@gmail.com>
2022-04-06 00:29:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import assets from './assets';
+import { SectionWrapper } from './components';
+
+const App = () => {
+  return (
+    <>
+      <SectionWrapper
+        title='You won store of Nifty NFTs. Start Selling & GRowing'
+        description='Buy, Store, collect NFTS, exchange & earn crypto. Join 25+ million people using Profnet Marketplace'
+        showBtn
+        mockupImg={assets.homeHero}
+        banner='banner'
+      />
+    </>
+  );
+};
+
+export default App;
