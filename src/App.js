bf41a01689b6e9dc54eea65cae31cd4b1cf2fbc5
Elioenay <elionay407@gmail.com>
2022-04-06 00:43:17 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ const App = () => {
         mockupImg={assets.homeHero}
         banner='banner'
       />
+      <SectionWrapper
+        title='You won store of Nifty NFTs. Start Selling & GRowing'
+        description='Buy, Store, collect NFTS, exchange & earn crypto. Join 25+ million people using Profnet Marketplace'
+        mockupImg={assets.homeCards}
+        reverse
+      />
     </>
   );
 };
